b7393fbac65d890d75b04b47b0f2a0b2723cfef8
jamaldemir <jamal@cybertekschool.com>
2023-03-05 17:23:34 -0500

    lab-05-thymeleaf

---
+++ lab-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/Lab05ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring05thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab05ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab05ThymeleafApplication.class, args);
+    }
+
+}
